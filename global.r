b68ca3292eb2b2889337b33b4712ec39dc778edf
shree <shrinidhee@gmail.com>
2020-02-29 23:26:51 -0500

    image show/hide logic updated

--- global.r
+++ global.r
@@ -10,9 +10,13 @@ doorUI <- function(id) {
     img(src = "door1000.png", height ="100%", width = "100%", id = ns("img_door")),
     hidden(img(src = "donkey1000.png", height = "100%", width = "100%", id = ns("img_donkey"))),
     hidden(img(src = "prize1000.png", height = "100%", width ="100%", id = ns("img_prize"))),
-    splitLayout(align = "center",
-      actionButton(ns("choice_1"), "Choice 1"),
-      disabled(actionButton(ns("choice_2"), "Choice 2"))
+    fluidRow(
+      column(6, align = "right",
+        actionButton(ns("choice_1"), "Choice 1")
+      ),
+      column(6, align = "left",
+        disabled(actionButton(ns("choice_2"), "Choice 2"))
+      )
     )
   )
 }
@@ -33,10 +37,19 @@ door <- function(input, output, session, choice_number, door_id,
   observe({
     if(choice_number() == 1) disable("choice_1")
     if(choice_number() == 1) enable("choice_2")
-    if(choice_number() == 2) disable("choice_2")
     if(door_id %in% open_door()) {
       hide("img_door")
       show("img_donkey")
+    }
+    if(choice_number() == 2) {
+      disable("choice_2")
+      if(door_id == prize_door()) {
+        hide("img_door")
+        show("img_prize")
+      } else {
+        hide("img_door")
+        show("img_donkey")        
+      }
     }
   })
   
@@ -48,6 +61,7 @@ door <- function(input, output, session, choice_number, door_id,
     door_choice$second <- NULL
     prize_door(sample(3, 1))
     hide("img_donkey")
+    hide("img_prize")
     show("img_door")
   })
 }
